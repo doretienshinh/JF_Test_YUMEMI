808ab2a0435d48c980174ea015c0aa072e8b818a
Duy Linh <doretienshinh2@gmail.com>
2021-09-24 20:19:31 +0700

    add highchart, multiLine done, deleteLine done

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -5,10 +5,19 @@
         type="checkbox"
         v-bind:id="province.prefCode"
         v-bind:value="province.prefName"
+        v-on:change="provinceSelected($event)"
       />
       <label v-bind:for="province.prefCode">{{ province.prefName }}</label
       ><br />
     </li>
+    <div>
+      <highcharts
+        v-if="chartShow"
+        class="hc"
+        :options="chartOptions"
+        ref="chart"
+      ></highcharts>
+    </div>
   </div>
 </template>
 
@@ -18,6 +27,28 @@ export default {
   data() {
     return {
       provinces: null,
+      chartShow: false,
+      arrayData: [],
+      chartOptions: {
+        series: [],
+        title: {
+          text: "人口チャート",
+        },
+        chart: {
+          height: 500,
+          type: "line",
+        },
+        yAxis: {
+          title: {
+            text: "人口数",
+          },
+        },
+        xAxis: {
+          title: {
+            text: "年度",
+          },
+        },
+      },
     };
   },
   methods: {
@@ -30,17 +61,50 @@ export default {
         })
         .then((response) => {
           this.provinces = response.data.result;
-          console.log(this.provinces);
         });
-    }
+    },
+    provinceSelected: function (event) {
+      axios
+        .get(
+          "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=" +
+            event.target.id,
+          {
+            headers: {
+              "X-API-KEY": "mWoSgpkUSvaG4xrQy3VGp9o3sh8QjfIBHDLDtyGe",
+            },
+          }
+        )
+        .then((response) => {
+          this.arrayData = [];
+          for (var i = 0; i < response.data.result.data[0].data.length; i++) {
+            this.arrayData.push([
+              response.data.result.data[0].data[i].year,
+              response.data.result.data[0].data[i].value,
+            ]);
+          }
+          this.chartOptions.series.push({
+            name: event.target.value,
+            data: this.arrayData,
+            id: event.target.id,
+          });
+          if (this.chartOptions.series.length > 0) this.chartShow = true;
+        });
+    },
   },
   created() {
     this.loadingAllProvince();
   },
 };
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  li {
-    list-style-type: none;
-  }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 </style>
